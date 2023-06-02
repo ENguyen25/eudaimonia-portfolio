41cb3e91f21f9dcf8996cfba953b30f9555e5c97
Evelyn Nguyen <enguyen6208@gmail.com>
2023-06-01 23:10:16 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { createClient } from 'contentful';
 
 function App() {
+
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const client = createClient({space: "auxbdc5uqthn", accessToken: "fg4gnvrqr0Fi8tUtBSZ3ic5aiSkf-hU6nYBHvTYcGdc"})
+
+  useEffect(() => {
+    const getAllEntries = async () => {
+      try {
+        await client.getEntries().then((entries) => {
+          console.log(entries)
+          setBlogPosts(entries)
+        })
+      } catch (error) {
+        console.log("error")
+      }
+    }
+
+    getAllEntries();
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          testing testing
         </p>
         <a
           className="App-link"
