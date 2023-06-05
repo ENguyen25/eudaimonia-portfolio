eaca5049c80544b23dfea792f5009b19ab6baeec
Evelyn Nguyen <enguyen6208@gmail.com>
2023-06-04 18:17:08 -0700

    built responsive about page + responsive navbar

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+
+import "../../css/mystyles.css";
+import "../../css/customstyle.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="container">
+      <div className="columns">
+        <div className="column is-three-fifths about-first-column">
+          <section className="section">
+            <h1 className="title header-1">About Me</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </section>
+          <section className="section">
+            <h2 className="title header-2">My Philosophy</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </section>
+          <section className="section">
+            <h2 className="title header-2">My Vision</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </section>
+        </div>
+        <div className="column about-second-column">
+          <picture>
+            <source media="(max-width: 769px)" srcset="https://images.unsplash.com/photo-1625980953551-50502b3b8b2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGxvdHVzJTIwZmxvd2VyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" />
+            <img src="https://images.unsplash.com/photo-1562647017-7d25504253a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGxvdHVzJTIwZmxvd2VyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="lotus flower" />
+          </picture>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import "./script.js"
 
 const Header = () => {
+
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <>
-      <nav className="navbar is-fixed-top">
-        <div className="navbar-menu">
+      <nav
+        className="navbar is-fixed-top"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <a onClick={() => {setIsActive(!isActive)}}
+            role="button"
+            className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
           <div className="navbar-start">
-            <div className="navbar-item">
-              <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="Bulma" />
-            </div>
+            <a className="navbar-item" href="https://bulma.io">
+              <img
+                src="https://bulma.io/images/bulma-logo.png"
+                width="112"
+                height="28"
+              />
+            </a>
           </div>
+
           <div className="navbar-end">
-            <a href="/about" className="navbar-item nav-links">about</a>
-            <a href="/work" className="navbar-item nav-links">work</a>
-            <a href="/blog" className="navbar-item nav-links">blog</a>
-            <a href="/contact" className="navbar-item nav-links">contact</a>
+            <a class="navbar-item nav-links">about</a>
+            <a class="navbar-item nav-links">work</a>
+            <a class="navbar-item nav-links">blog</a>
+            <a class="navbar-item nav-links">contact</a>
           </div>
         </div>
       </nav>
