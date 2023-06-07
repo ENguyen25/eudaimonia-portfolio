7064ba64d64ab4fec358e06938108ca0dc66a710
Evelyn Nguyen <enguyen6208@gmail.com>
2023-06-07 12:03:42 -0700

    home page: responsive image overlay

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <>
-      <div className="container general-container">
+    <div className="container general-container">
+      <section className="section">
         <div className="columns">
           <div className="column is-three-fifths">
             <section className="section">
@@ -26,10 +26,11 @@ const Home = () => {
             </section>
           </div>
         </div>
-      </div>
-      <div className="container">
+      </section>
+      <section className="section">
+        <h1 className="title header-2">Web Design & Development</h1>
         <div className="columns">
-          <div className="column is-half">
+          <div className="column is-half project-column">
             <div className="overlay-container">
               <img className="project-thumbnail" src="https://images.unsplash.com/photo-1455218873509-8097305ee378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature landscape" />
               <div className="overlay">
@@ -43,7 +44,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="column is-half">
+          <div className="column is-half project-column">
             <div className="overlay-container">
               <img className="project-thumbnail" src="https://images.unsplash.com/photo-1455218873509-8097305ee378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature landscape" />
               <div className="overlay">
@@ -58,8 +59,41 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </section>
+      <section className="section">
+        <h1 className="title header-2">Entrepreneurship & Business Development</h1>
+        <div className="columns">
+          <div className="column is-half project-column">
+            <div className="overlay-container">
+              <img className="project-thumbnail" src="https://images.unsplash.com/photo-1455218873509-8097305ee378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature landscape" />
+              <div className="overlay">
+                <p>Ay yo mama</p>
+              </div>
+            </div>
+            <div className="overlay-container">
+              <img className="project-thumbnail" src="https://images.unsplash.com/photo-1455218873509-8097305ee378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature landscape" />
+              <div className="overlay">
+                <p>Ay yo mama</p>
+              </div>
+            </div>
+          </div>
+          <div className="column is-half project-column">
+            <div className="overlay-container">
+              <img className="project-thumbnail" src="https://images.unsplash.com/photo-1455218873509-8097305ee378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature landscape" />
+              <div className="overlay">
+                <p>Ay yo mama</p>
+              </div>
+            </div>
+            <div className="overlay-container">
+              <img className="project-thumbnail" src="https://images.unsplash.com/photo-1455218873509-8097305ee378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="nature landscape" />
+              <div className="overlay">
+                <p>Ay yo mama</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
